fe8dfc7cedf75cff6a795642ef8dc961c2c4337a
Iago Lima <iago.lopes1@hotmail.com>
2020-12-27 12:49:25 -0300

    Incluindo a Classe Worker

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -73,20 +73,20 @@ public class Worker {
     contracts.remove(contract);
   }
 
-  public Double income (Integer year, Integer month){
+  public double income (int year, int month){
     
     double sum = baseSalary;
     Calendar cal = Calendar.getInstance();
 
     for (HourContract eachContract : contracts) {
-      int c_year =
-      int c_month =
-      if (){
+      cal.setTime(eachContract.getDate());
+      int c_year = cal.get(Calendar.YEAR);
+      int c_month = 1+cal.get(Calendar.MONTH);//acrescentado 1, pois o mês no Calendar começa com 0
+      if (year == c_year && month == c_month){
         sum += eachContract.totalValue();
       }
-      
     }
-
+    return sum;
   }
 
 }
