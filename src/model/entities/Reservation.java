a62877202482ffc6b154d956f0c70589d1aa6ea5
Iago Lima <iago.lopes1@hotmail.com>
2020-12-30 18:16:16 -0300

    Very Bad Solution to msg Exception

--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -10,7 +10,7 @@ public class Reservation {
   private Date checkIn;
   private Date checkOut;
 
-  SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");
+  private static SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");
 
   public Reservation (){}
 
@@ -37,14 +37,26 @@ public class Reservation {
   }
   
   public void updateDates (Date checkIn, Date checkOut ){
-    Date now = new Date();
     this.checkIn = checkIn;
 		this.checkOut = checkOut;
   }
   
   public long duration() {
-    long diff = checkOut.getTime() - checkIn.getTime();
-		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
+    long diff = checkOut.getTime() - checkIn.getTime();//pegando a diferença entre as data em milisegundos
+		return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS); // usado timeUnit para conversão de ms para dias
   }
+
+  @Override
+	public String toString() {
+		return "Room "
+			+ roomNumber
+			+ ", check-in: "
+			+ sdf.format(checkIn)
+			+ ", check-out: "
+			+ sdf.format(checkOut)
+			+ ", "
+			+ duration()
+			+ " nights";
+	}
 
 }
